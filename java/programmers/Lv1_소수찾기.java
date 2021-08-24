ccb2093ec0e4613ec1ea0bb292d4147058a88515
ychxexn <81912261+ychxexn@users.noreply.github.com>
2021-08-25 00:47:02 +0900

    Create Lv1_소수찾기.java

---
+++ java/programmers/Lv1_소수찾기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        
+        for(int i=3; i<=n; i++){
+            if(isPrime(i)){
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    public boolean isPrime(int n){
+        for(int i=2; i<=Math.sqrt(n); i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
