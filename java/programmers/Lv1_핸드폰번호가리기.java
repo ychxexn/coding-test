ac9a83051424cf25bb04a12f16a76e895e162cc1
ychxexn <81912261+ychxexn@users.noreply.github.com>
2021-08-25 00:52:13 +0900

    Create Lv1_핸드폰번호가리기.java

---
+++ java/programmers/Lv1_핸드폰번호가리기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        
+        answer = phone_number.replaceAll("[0-9]", "*").substring(0, phone_number.length()-4) + phone_number.substring(phone_number.length()-4);
+        
+        return answer;
+    }
+}
