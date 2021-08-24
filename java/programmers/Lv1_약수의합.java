d86c2c400ab4a87f2051be9f5819c25ba57e7ff6
ychxexn <81912261+ychxexn@users.noreply.github.com>
2021-08-25 00:48:06 +0900

    Create Lv1_약수의합.java

---
+++ java/programmers/Lv1_약수의합.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = n;
+        
+        for(int i=1; i<=n/2; i++){
+            if(n % i == 0){
+                answer += i;
+            }
+        }
+        
+        return answer;
+    }
+}
