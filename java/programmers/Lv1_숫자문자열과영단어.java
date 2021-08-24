466dda73291962572199222f9a11da2a4ddc9b02
ychxexn <81912261+ychxexn@users.noreply.github.com>
2021-08-25 00:37:33 +0900

    Create Lv1_숫자문자열과영단어.java

---
+++ java/programmers/Lv1_숫자문자열과영단어.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String s) {
+        String numbers[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        
+        int answer = 0;
+        
+        for(int i=0; i<numbers.length; i++){
+            s = s.replaceAll(numbers[i], Integer.toString(i));
+        }
+        
+        answer = Integer.parseInt(s);
+        
+        return answer;
+    }
+}
