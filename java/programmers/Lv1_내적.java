63145b3e7478f1452ce016a0876a2a4fcbe3d259
ychxexn <81912261+ychxexn@users.noreply.github.com>
2021-08-25 00:38:25 +0900

    Create Lv1_내적.java

---
+++ java/programmers/Lv1_내적.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        
+        int length = a.length;
+        
+        for(int i=0; i<length; i++){
+            answer += a[i] * b[i];
+        }
+        
+        return answer;
+    }
+}
