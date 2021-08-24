28283625d695d47c1c7aa4b693d1c5593d451b79
ychxexn <81912261+ychxexn@users.noreply.github.com>
2021-08-25 00:39:13 +0900

    Create Lv1_완주하지못한선수.java

---
+++ java/programmers/Lv1_완주하지못한선수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        answer = participant[participant.length-1];
+        for(int i=0; i<completion.length; i++){
+            if(!participant[i].equals(completion[i])){
+                answer = participant[i];
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
