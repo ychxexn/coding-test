6957d7541938f459b083676c113437628bec57c1
ychxexn <81912261+ychxexn@users.noreply.github.com>
2021-10-01 18:16:16 +0900

    Create MaxCounters.java

---
+++ java/Codility/MaxCounters.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int N, int[] A) {
+        int[] answer = new int[N];
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0; i<A.length; i++){
+            if(1 <= A[i] && A[i] <= N){
+                answer[A[i]-1]++;
+                max = (max > answer[A[i]-1]) ? max : answer[A[i]-1];
+            }else if(A[i] == N+1){
+                Arrays.fill(answer, max);
+            }
+        }
+
+        return answer;
+    }
+}
