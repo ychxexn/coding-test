d9c98f672392c3119567c3ef76d8060788c0bfb0
ychxexn <81912261+ychxexn@users.noreply.github.com>
2022-01-12 20:19:41 +0900

    Create merge-sorted-array.java

---
+++ java/LeetCode/merge-sorted-array.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int index = 0;
+        for(int i=m; i<nums1.length; i++){
+            nums1[i] = nums2[index++];
+        }
+        
+        Arrays.sort(nums1);
+    }
+}
